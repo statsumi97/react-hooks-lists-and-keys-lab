581c17ae74d032d47e7815266ff5c1c31d5fb1d7
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-11-28 12:38:58 -0500

    final product

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,11 +4,13 @@ import ProjectItem from "./ProjectItem";
 function ProjectList({ projects }) {
   console.log(projects);
   return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+    <div>
+      <h1 className='projects-heading'>My Projects</h1>
+      {projects.map((project) => (
+        <ProjectItem key={project.name} {...project} />
+      ))}
     </div>
-  );
-}
+  )
+  }
 
 export default ProjectList;
